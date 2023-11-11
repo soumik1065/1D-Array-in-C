13789e00b2ca66de17bde9a65ac9c397b0b9f38d
Soumik Das <114335541+soumik1065@users.noreply.github.com>
2023-11-11 20:24:04 +0530

    Update 4. Program_3.c

--- 4. Program_3.c
+++ 4. Program_3.c
@@ -3,24 +3,27 @@
 #include<stdio.h>
 void main()
 {
-	int arr[5];
-
-	printf("Enter the elements:\n");
-	for (int i=0; i<5; i++)
-	{
-		scanf("%d\n", &arr[i]);
-	}
-
-	printf("\nElements in correct order: ");
-	for (int i=0; i<5; i++)
-	{
-		printf("%d\t", arr[i]);
-	}
-
-	printf("\nElements in reverse order: ");
-	for (int i=5; i>=0; i--)
-	{
-		printf("%d\t", arr[i]);
-	}
+	int arr[100],n;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    
+    printf("\nEnter the elements in the array: ");
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d\n", &arr[i]);
+    }
+    
+    printf("\nElements of array in the correct order: ");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    
+    printf("\nElements of  array in reverse order: ");
+    for(int i=n; i>=0; i--)
+    {
+        printf("%d\t", arr[i]);
+    }
 	
 }
